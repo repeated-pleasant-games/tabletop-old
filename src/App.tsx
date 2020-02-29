de4440854cf3364d9aace358cd6b68717240f8f1
Joseph R Miles <me@josephrmiles.com>
2020-02-29 07:58:56 -0800

    Implemented SVG grid pattern

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,54 @@ import * as React from "react"
 
 export class App extends React.Component<{}, {}> {
     public render () {
+
+        const gridDimension = 128
+        const subgridDimension = gridDimension / 4
+
         return <div id="vtt">
             <svg>
+                <defs>
+                    <pattern
+                        id="pattern-subgrid"
+                        width={subgridDimension}
+                        height={subgridDimension}
+                        patternUnits="userSpaceOnUse"
+                        >
+                        <path
+                            d={`M ${subgridDimension} 0
+                                L 0 0 0 ${subgridDimension}`}
+                            fill="none"
+                            stroke="grey"
+                            stroke-width="0.5"
+                            />
+                    </pattern>
+
+                    <pattern
+                        id="pattern-grid"
+                        width={gridDimension}
+                        height={gridDimension}
+                        patternUnits="userSpaceOnUse"
+                        >
+                        <rect
+                            width={gridDimension}
+                            height={gridDimension}
+                            fill="url(#pattern-subgrid)"
+                            />
+                        <path
+                            d={`M ${gridDimension} 0
+                                L 0 0 0 ${gridDimension}`}
+                            fill="none"
+                            stroke="grey"
+                            stroke-width="1"
+                            />
+                    </pattern>
+                </defs>
+
+                <rect
+                    width="100%"
+                    height="100%"
+                    fill="url(#pattern-grid)"
+                    />
             </svg>
         </div>
     }
