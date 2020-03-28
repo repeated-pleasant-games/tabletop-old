deb3ce6c1698bb27ecfe828b2c6da1dee6c9e84b
Joseph R Miles <me@josephrmiles.com>
2020-03-28 09:57:59 -0700

    Created app state store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+
+import appState from "./reducers"
 
 import { App } from "./App"
 
 import "./styles/style.sass"
 
+
 ReactDOM.render(
-    <App />,
+    <Provider store={createStore(appState)}>
+        <App />
+    </Provider>,
     document.getElementById("app")
 )
