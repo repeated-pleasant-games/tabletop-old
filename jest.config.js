44df8ae575c08b9b9a353828dadd3a9556f2d3d3
Joseph R Miles <joe@jrm.dev>
2020-11-07 08:21:01 -0800

    Updated test name pattern to be more flexible

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     transform: {"^.+\\.tsx?$": "ts-jest"},
     testEnvironment: "node",
-    testRegex: "/tests/.*\\.test\\.tsx?$",
+    testRegex: ".*\\.(test|spec)\\.tsx?$",
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     moduleNameMapper: {
       "^~(.*)$": "<rootDir>/src$1"
