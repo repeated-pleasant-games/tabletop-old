0239913782a0ce9214a5dfec56285d7cde3e5b56
Joseph R Miles <joe@jrm.dev>
2020-11-07 08:04:40 -0800

    Added Control Panel specific contaners into Control Panel file

--- src/components/vtt/ControlPanel.tsx
+++ src/components/vtt/ControlPanel.tsx
@@ -1,7 +1,62 @@
 import * as React from "react"
+import { connect } from "react-redux"
 
-import GridSnapCheckbox from "~/containers/GridSnapCheckbox"
-import AddActorButton from "~/containers/AddActorButton"
+import { setGridSnap } from "~/actions/vtt"
+import { addActor } from "~/actions/vtt"
+import { VttState } from "~/reducers/vtt"
+
+import { Actor } from "~/core/Actor"
+
+import Checkbox from "~/components/util/Checkbox"
+import Button, { ButtonProps } from "~/components/util/Button"
+
+
+const mapGridSnapToProps = ({ gridSnap }: VttState) => (
+    { checked: gridSnap }
+)
+
+const mapGridSnapDispatchToProps = (dispatch: any) => (
+    {
+        onChange: (e: React.ChangeEvent) =>
+            dispatch(setGridSnap((e.target as HTMLInputElement).checked))
+    }
+)
+
+const GridSnapCheckbox = connect(
+  mapGridSnapToProps,
+  mapGridSnapDispatchToProps)(Checkbox)
+
+
+const mapNextIdToProps = ({ actors }: VttState) => (
+    { nextId: actors.length + 1 }
+)
+
+type NextIdProps = ReturnType<typeof mapNextIdToProps>
+
+const mapAddActorDispatchToProps = (dispatch: any) => (
+    {
+        addActor: (id: number) =>
+            dispatch(addActor(new Actor(id, `Actor ${id}`)))
+    }
+)
+
+type AddActorDispatchProps = ReturnType<typeof mapAddActorDispatchToProps>
+
+const mergeProps = (
+    { nextId }: NextIdProps,
+    { addActor }: AddActorDispatchProps,
+    ownProps: ButtonProps
+) => (
+    {
+        onClick: (e: React.MouseEvent) => addActor(nextId),
+        ...ownProps
+    }
+)
+
+const AddActorButton = connect(
+    mapNextIdToProps,
+    mapAddActorDispatchToProps,
+    mergeProps)(Button)
 
 
 export default () => (
