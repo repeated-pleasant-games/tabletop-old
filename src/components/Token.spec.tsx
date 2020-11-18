57b4120d1aa1f653b61981e9a230e15adf8b5b90
Joseph R Miles <joe@jrm.dev>
2020-11-17 18:02:00 -0800

    Created renderSVG() test helper

--- src/components/Token.spec.tsx
+++ src/components/Token.spec.tsx
@@ -1,26 +1,29 @@
 import * as React from "react";
-import { render } from "@testing-library/react";
+import { render, RenderOptions, RenderResult } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import { Token } from "~/components/vtt/Token";
 
-describe(
-  "The Token component",
-  () =>{
-    it("Is a circle", () => {
-      const { container } = render(
-          <Token
-            actor={null}
-            cellDimension={25}
-            vttTransform={[1,0,0,1,0,0]}
-          />,
-          {
-            container:
-              document.body.appendChild(
-                document.createElementNS("http://www.w3.org/2000/svg", "svg")),
-          });
+const renderSVG = (
+  ui: React.ReactElement,
+  options?: RenderOptions
+): RenderResult =>
+  render(
+    ui,
+    {
+      container:
+        document.body.appendChild(
+          document.createElementNS("http://www.w3.org/2000/svg", "svg")),
+      ...options,
+    });
 
-      expect(container.firstChild.nodeName).toBe("circle");
-    })
-  }
-);
+test("Is a circle", () => {
+  const { container } = renderSVG(
+    <Token
+      actor={null}
+      cellDimension={25}
+      vttTransform={[1,0,0,1,0,0]}
+    />);
+
+  expect(container.firstChild.nodeName).toBe("circle");
+});
