5d6ba6244c3d54556fcdaf0794f53a9ed3a1a59e
Joseph R Miles <joe@jrm.dev>
2020-11-28 08:16:04 -0800

    :pencil2: Changed import path for Tabletop component

--- src/components/tabletop/Tabletop.spec.tsx
+++ src/components/tabletop/Tabletop.spec.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-import { Tabletop } from "~/components/tabletop/Tabletop";
+import { Tabletop } from "./Tabletop";
 
 describe("Tabletop component", () =>
 {
