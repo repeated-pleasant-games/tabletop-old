0c56c48d04cbe166cdeddfc1cbf8500f15c02025
Joseph R Miles <joe@jrm.dev>
2020-11-28 09:05:27 -0800

    :recycle: Extracted renderSVG as utility

--- src/components/tabletop/Grid.spec.tsx
+++ src/components/tabletop/Grid.spec.tsx
@@ -1,21 +1,8 @@
 import * as React from "react";
-import { render, RenderOptions, RenderResult } from "@testing-library/react";
+import { renderSVG } from "~/test-utilities";
 import "@testing-library/jest-dom/extend-expect";
 
 import { Grid } from "./Grid";
-
-const renderSVG = (
-  ui: React.ReactElement,
-  options?: RenderOptions
-): RenderResult =>
-  render(
-    ui,
-    {
-      container:
-        document.body.appendChild(
-          document.createElementNS("http://www.w3.org/2000/svg", "svg")),
-      ...options,
-    });
 
 describe("Grid component", () =>
 { 
