fc26fce15a9e706afa19440b832cd05cb4489e97
Joseph R Miles <joe@jrm.dev>
2020-12-15 17:03:01 -0800

    :white_check_mark: Added check that Grid set view transform on click and drag.

--- src/components/tabletop/Grid.spec.tsx
+++ src/components/tabletop/Grid.spec.tsx
@@ -1,21 +1,65 @@
 import * as React from "react";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import { act, fireEvent } from "@testing-library/react";
 import { renderSVG } from "~/test-utilities";
 import "@testing-library/jest-dom/extend-expect";
 
-import { Grid, gridTestId } from "./Grid";
+import Grid, { Grid as DisconnectedGrid, gridTestId } from "./Grid";
+import { viewTransform } from "~/reducers/tabletop";
 
-describe("Grid component", () =>
+if (!global.PointerEvent)
+{
+  class PointerEvent extends MouseEvent
+  {
+    public pointerId?: number;
+    public pointerType?: string;
+
+    public isPrimary?: boolean;
+
+    public height?: number;
+    public width?: number;
+
+    public pressure?: number;
+    public tangentialPressure?: number;
+
+    public tiltX?: number;
+    public tiltY?: number;
+
+    public twist?: number;
+
+    constructor(type: string, params: PointerEventInit = {})
+    {
+      super(type, params);
+
+      this.pointerId = params.pointerId;
+      this.pointerType = params.pointerType;
+      this.isPrimary = params.isPrimary;
+      this.height = params.height;
+      this.width = params.width;
+      this.pressure = params.pressure;
+      this.tangentialPressure = params.tangentialPressure;
+      this.tiltX = params.tiltX;
+      this.tiltY = params.tiltY;
+      this.twist = params.twist;
+    }
+  }
+
+  global.PointerEvent = PointerEvent as any;
+}
+
+describe("Stateless Grid component", () =>
 { 
   it("Has a SVG rect.", () =>
   {
-    const { getByTestId } = renderSVG(<Grid patternId={null} />);
+    const { getByTestId } = renderSVG(<DisconnectedGrid patternId={null} />);
 
     expect(getByTestId(gridTestId).nodeName).toBe("rect");
   });
 
   it("Spans the full height and width of the Tabletop", () => 
   {
-    const { getByTestId } = renderSVG(<Grid patternId={null} />);
+    const { getByTestId } = renderSVG(<DisconnectedGrid patternId={null} />);
 
     const gridRect = getByTestId(gridTestId);
 
@@ -25,7 +69,9 @@ describe("Grid component", () =>
 
   it("Filled with the pattern set by patternId.", () =>
   {
-    const { getByTestId } = renderSVG(<Grid patternId="some-pattern-id" />);
+    const { getByTestId } = renderSVG(
+      <DisconnectedGrid patternId="some-pattern-id" />
+    );
 
     expect(getByTestId(gridTestId)).toHaveAttribute(
       "fill",
@@ -34,8 +80,38 @@ describe("Grid component", () =>
 
   it("Has fill 'none' when patternId is null.", () =>
   {
-    const { getByTestId } = renderSVG(<Grid patternId={null} />);
+    const { getByTestId } = renderSVG(<DisconnectedGrid patternId={null} />);
 
     expect(getByTestId(gridTestId)).toHaveAttribute("fill", "none");
+  });
+});
+
+describe("Connected Grid component", () =>
+{
+  it("Changes view transform when users clicks and drags.", () =>
+  {
+    const deltaX = 10, deltaY = 13;
+
+    const store = createStore(combineReducers({ viewTransform }));
+
+    const { getByTestId } = renderSVG(
+      <Provider store={store}>
+        <Grid patternId={null} />
+      </Provider>
+    );
+
+    const grid = getByTestId(gridTestId);
+
+    act(() =>
+    {
+      fireEvent.pointerDown(grid);
+
+      fireEvent.pointerMove(
+        grid,
+        { clientX: deltaX, clientY: deltaY });
+    });
+
+    expect(store.getState().viewTransform)
+      .toStrictEqual([ 1, 0, 0, 1, deltaX, deltaY ]);
   });
 });
--- src/components/tabletop/Grid.tsx
+++ src/components/tabletop/Grid.tsx
@@ -1,17 +1,39 @@
 import * as React from "react";
+import { connect } from "react-redux";
+
+import { setViewTransform, SetViewTransformPayload } from "~/actions/tabletop";
 
 type GridProps = React.HTMLAttributes<{}> &
 {
   patternId: string,
-}
+  setViewTransform?: (x: number, y: number) => SetViewTransformPayload
+};
 
 export const gridTestId = "grid-rect";
 
-export const Grid = ({ patternId }: GridProps) => (
+export const Grid = ({ patternId, setViewTransform }: GridProps) => (
   <rect
     data-testid={gridTestId}
     width="100%"
     height="100%"
     fill={ patternId === null ? "none" : `url(#${patternId})` }
+    onPointerMove={
+      ({ clientX, clientY }) =>
+      {
+        setViewTransform(clientX, clientY);
+      }}
   />
 );
+
+const stateToProps = ({ viewTransform }: { viewTransform: number[] }) =>
+({
+  viewTransform,
+});
+
+const dispatchToProps = (dispatch: any) =>
+({
+  setViewTransform: (x: number, y: number) =>
+    dispatch(setViewTransform(x, y)),
+});
+
+export default connect(stateToProps, dispatchToProps)(Grid);
