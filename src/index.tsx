dc24d60e31ddaeafac626bc7b0093bb4e6b139e8
Joseph R Miles <joe@jrm.dev>
2020-12-15 19:43:19 -0800

    :recycle: Updated app store to use new reducers

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { Provider } from "react-redux"
-import { createStore } from "redux"
+import { combineReducers, createStore } from "redux"
 
-import vttReducer from "./reducers/vtt"
+import { viewTransform } from "./reducers/tabletop";
 
 import { App } from "./App"
 
@@ -11,7 +11,7 @@ import "./styles/style.css"
 
 
 ReactDOM.render(
-    <Provider store={createStore(vttReducer)}>
+    <Provider store={createStore(combineReducers({ viewTransform }))}>
         <App />
     </Provider>,
     document.getElementById("app")
