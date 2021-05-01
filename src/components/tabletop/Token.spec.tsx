b1a0bf2d1af4bb440895577bf834c546dea37dd0
Joseph R Miles <joe@jrm.dev>
2021-04-30 19:14:24 -0700

    :white_check_mark: Tokens have x and y coordinates

--- src/components/tabletop/Token.spec.tsx
+++ src/components/tabletop/Token.spec.tsx
@@ -11,8 +11,16 @@ describe("Disconnected Token", () =>
 {
   it("Has an SVG circle.", () =>
   {
-    const { getByTestId } = renderSVG(<Token />);
+    const { getByTestId } = renderSVG(<Token x={0} y={0} />);
 
-    expect(getByTestId(tokenTestId).nodeName).toEqual("circle");
+    expect(getByTestId(tokenTestId).nodeName).toEqual("rect");
+  });
+
+  it("Has given x and y coordinates.", () =>
+  {
+    const { getByTestId } = renderSVG(<Token x={10} y={11} />);
+
+    expect(getByTestId(tokenTestId)).toHaveAttribute("x", "10");
+    expect(getByTestId(tokenTestId)).toHaveAttribute("y", "11");
   });
 });
--- src/components/tabletop/Token.tsx
+++ src/components/tabletop/Token.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 
 export const tokenTestId = "token";
 
-export const Token = (): any => (
-  <circle data-testid={tokenTestId} />
+export const Token = ({ x, y }: { x: number, y: number }): any =>
+(
+  <rect data-testid={tokenTestId} x={x} y={y} />
 );
 
 export default Token;
