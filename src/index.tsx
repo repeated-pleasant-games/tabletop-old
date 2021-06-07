3d0667866920d78d72544f973654643002fbf220
Joseph R Miles <joe@jrm.dev>
2021-06-06 19:33:18 -0700

    refactor: removed doubled up provider from index

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import { Provider } from "react-redux"
-import { combineReducers, createStore } from "redux"
-
-import { viewTransform } from "./reducers/tabletop";
 
 import { App } from "./App"
 
@@ -11,8 +7,6 @@ import "./styles/style.css"
 
 
 ReactDOM.render(
-    <Provider store={createStore(combineReducers({ viewTransform }))}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById("app")
 )
