328a6f783da9c49f2e375303b880a19e2e0c8963
Joseph R Miles <joe@jrm.dev>
2021-06-07 19:02:12 -0700

    :sparkles: App displays ControlPanel

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import * as React from "react"
 import { Provider } from "react-redux";
 import { combineReducers, createStore } from "redux";
 
-import { Tabletop } from "~/components/tabletop/Tabletop";
+import Tabletop from "~/components/tabletop/Tabletop";
 import { RectangularGrid } from "~/components/tabletop/RectangularGrid";
 
 import { viewTransform, actors } from "./reducers/tabletop";
+import ControlPanel from "./components/control-panel/ControlPanel";
 
 export const App = () =>
 (
   <Provider store={createStore(combineReducers({ viewTransform, actors }))}>
+    <ControlPanel />
     <Tabletop grid={<RectangularGrid />} />
   </Provider>
 );
