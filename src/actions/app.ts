8b8488a6b1d4a05f3f75dd0f137b7c19b3c8eb12
Joseph R Miles <joe@jrm.dev>
2021-07-01 11:05:20 -0700

    :recycle: Imported shared theme selection and color setting logic into app actions

--- src/actions/app.ts
+++ src/actions/app.ts
@@ -1,4 +1,5 @@
 import { ThunkAction } from "redux-thunk";
+import { getColorMode, setColors } from "~/utility/theme";
 
 export type SetThemePayload =
 {
@@ -9,62 +10,6 @@ export const setThemePreference = (theme: string): ThunkAction<unknown, {}, {}, 
   (dispatch) =>
   {
     window.localStorage.setItem("themePreference", theme);
-
-    const getColorMode = (themePreference: string) =>
-    {
-      switch (themePreference)
-      {
-        case "light":
-        case "dark":
-          return themePreference;
-
-        case "system":
-        default:
-        {
-          const query = window.matchMedia("(prefers-color-scheme: dark)");
-          const hasThemePreference = typeof query.matches === 'boolean';
-
-          if (hasThemePreference)
-            return query.matches ? "dark" : "light";
-
-          else
-            return "light";
-        }
-      }
-    };
-
-    const setColors = (colorMode: string) =>
-    {
-      const root = window.document.documentElement;
-
-      root.style.setProperty(
-        `--color-fg`,
-        colorMode === "light"
-        ? `black`
-        : `white`
-      );
-
-      root.style.setProperty(
-        `--color-bg`,
-        colorMode === "light"
-        ? `white`
-        : `black`
-      );
-
-      root.style.setProperty(
-        `--color-grid-fg`,
-        colorMode === "light"
-        ? `black`
-        : `white`
-      );
-
-      root.style.setProperty(
-        `--color-grid-bg`,
-        colorMode === "light"
-        ? `white`
-        : `black`
-      );
-    };
 
     setColors(getColorMode(theme));
 
