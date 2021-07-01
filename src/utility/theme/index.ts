dc990347e6845f52b711c8e928a0f3fab63d646c
Joseph R Miles <joe@jrm.dev>
2021-07-01 12:00:16 -0700

    :recycle: Migrated all color controls to style sheets

--- src/utility/theme/index.ts
+++ src/utility/theme/index.ts
@@ -24,34 +24,4 @@ export const getColorMode = (themePreference: string) =>
 };
 
 export const setColors = (colorMode: string) =>
-{
-  const root = window.document.documentElement;
-
-  root.style.setProperty(
-    `--color-fg`,
-    colorMode === "light"
-    ? `black`
-    : `white`
-  );
-
-  root.style.setProperty(
-    `--color-bg`,
-    colorMode === "light"
-    ? `white`
-    : `black`
-  );
-
-  root.style.setProperty(
-    `--color-grid-fg`,
-    colorMode === "light"
-    ? `black`
-    : `white`
-  );
-
-  root.style.setProperty(
-    `--color-grid-bg`,
-    colorMode === "light"
-    ? `white`
-    : `black`
-  );
-};
+  window.document.documentElement.className = colorMode;
