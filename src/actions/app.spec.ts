b29e2f19d0b0a0fc9402a62c3692917cd1bf78af
Joseph R Miles <joe@jrm.dev>
2021-07-01 09:39:07 -0700

    :bug: Mock window.matchMedia to prevent error

--- src/actions/app.spec.ts
+++ src/actions/app.spec.ts
@@ -1,6 +1,20 @@
 import { ThunkDispatch } from "redux-thunk";
 import { setThemePreference } from "./app";
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 describe("setThemePreference", () =>
 {
   it.each([
