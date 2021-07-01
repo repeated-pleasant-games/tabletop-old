7e1591aeb427c12330d2f02c61af6e96fe3c8d1b
Joseph R Miles <joe@jrm.dev>
2021-07-01 09:40:20 -0700

    :sparkles: Theme colors now change when new theme preference is selected

--- src/actions/app.ts
+++ src/actions/app.ts
@@ -10,6 +10,64 @@ export const setThemePreference = (theme: string): ThunkAction<unknown, {}, {}, 
   {
     window.localStorage.setItem("themePreference", theme);
 
+    const getColorMode = (themePreference: string) =>
+    {
+      switch (themePreference)
+      {
+        case "light":
+        case "dark":
+          return themePreference;
+
+        case "system":
+        default:
+        {
+          const query = window.matchMedia("(prefers-color-scheme: dark)");
+          const hasThemePreference = typeof query.matches === 'boolean';
+
+          if (hasThemePreference)
+            return query.matches ? "dark" : "light";
+
+          else
+            return "light";
+        }
+      }
+    };
+
+    const setColors = (colorMode: string) =>
+    {
+      const root = window.document.documentElement;
+
+      root.style.setProperty(
+        `--color-fg`,
+        colorMode === "light"
+        ? `black`
+        : `white`
+      );
+
+      root.style.setProperty(
+        `--color-bg`,
+        colorMode === "light"
+        ? `white`
+        : `black`
+      );
+
+      root.style.setProperty(
+        `--color-grid-fg`,
+        colorMode === "light"
+        ? `black`
+        : `white`
+      );
+
+      root.style.setProperty(
+        `--color-grid-bg`,
+        colorMode === "light"
+        ? `white`
+        : `black`
+      );
+    };
+
+    setColors(getColorMode(theme));
+
     dispatch(
       ({
         type: "set theme",
