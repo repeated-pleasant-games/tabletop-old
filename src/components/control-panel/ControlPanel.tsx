e6d3dbcf0f5eff9f0c2fe3629690db83364ce862
Joseph R Miles <joe@jrm.dev>
2021-07-02 08:32:21 -0700

    :fire: Removed all unused code and imports from ControlPanel

--- src/components/control-panel/ControlPanel.tsx
+++ src/components/control-panel/ControlPanel.tsx
@@ -1,14 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
-import { ThunkDispatch } from "redux-thunk";
-import { v4 as uuid } from "uuid";
 
-import { addActor, removeActor, setSnapToGrid } from "~/actions/tabletop";
-import { Actor } from "~/core/Actor";
-import { setThemePreference } from "~/actions/app";
-import { Button } from "../util/Button";
-import { Checkbox } from "../util/Checkbox";
-import { Radio } from "../util/Radio";
 import AddActor from "./AddActor";
 import ActorList from "./ActorList";
 import SnapToGrid from "./SnapToGrid";
@@ -16,16 +7,7 @@ import ThemeSelect from "./ThemeSelect";
 
 export const controlPanelTestId = "control-panel";
 
-type ControlPanelProps =
-{
-  setThemePreference?: (theme: string) => void,
-  themePreference?: string,
-};
-
-export const ControlPanel = ({
-  setThemePreference,
-  themePreference,
-}: ControlPanelProps) =>
+export const ControlPanel = () =>
 (
   <div className="control-panel" data-testid={controlPanelTestId}>
     <section>
@@ -43,18 +25,4 @@ export const ControlPanel = ({
   </div>
 );
 
-const stateToProps = ({
-  themePreference,
-}: {
-  themePreference: string,
-}) =>
-({
-  themePreference,
-});
-
-const dispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>) =>
-({
-  setThemePreference: (theme: string) => dispatch(setThemePreference(theme)),
-});
-
-export default connect(stateToProps, dispatchToProps)(ControlPanel);
+export default ControlPanel;
