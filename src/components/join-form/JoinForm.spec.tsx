dbbf6a219e58d4176590ec50ae5533f8f5b518ee
Joseph R Miles <joe@jrm.dev>
2021-07-23 14:30:59 -0700

    :sparkles: Gave join form an input field for room anme

--- src/components/join-form/JoinForm.spec.tsx
+++ src/components/join-form/JoinForm.spec.tsx
@@ -8,10 +8,13 @@ describe("JoinForm component", () =>
 {
   it("Is a form.", () =>
   {
-    const { getByTestId } = render(
-      <JoinForm />
-    );
-
+    const { getByTestId } = render(<JoinForm />);
     expect(getByTestId(joinFormTestId).tagName.toLowerCase()).toBe("form");
+  });
+
+  it("Has an input field.", () =>
+  {
+    const { getByLabelText } = render(<JoinForm />);
+    expect((getByLabelText("Room Name") as HTMLInputElement).type).toBe("text");
   });
 });
--- src/components/join-form/JoinForm.tsx
+++ src/components/join-form/JoinForm.tsx
@@ -4,7 +4,19 @@ export const joinFormTestId = "join-form"
 
 export const JoinForm = () =>
 (
-  <form data-testid={joinFormTestId}></form>
+  <form data-testid={joinFormTestId}>
+    <label
+      id="room-name-label"
+    >
+      Room Name
+    </label>
+    <input
+      type="text"
+      name="room-name"
+      aria-labelledby="room-name-label"
+      id="room-name-input"
+    />
+  </form>
 );
 
 export default JoinForm;
