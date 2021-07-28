f4c6eb28bf1219d64b41cc66eb8a41def1a1c2ee
Joseph R Miles <joe@jrm.dev>
2021-07-28 12:21:26 -0700

    :recycle: Extracted theme select feature from control panel

--- src/feature/control-panel/component/ControlPanel.tsx
+++ src/feature/control-panel/component/ControlPanel.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { ActorPanel } from "@/feature/actor";
 import { SnapToGrid } from "@/feature/grid-snap";
-import ThemeSelect from "./ThemeSelect";
+import { ThemeSelect } from "@/feature/theme-select";
 
 import "./ControlPanel.module.css";
 
