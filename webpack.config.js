54dd1ef89937097dec95d719c528bfb5b19c5884
Joseph R Miles <joe@jrm.dev>
2021-07-29 07:34:49 -0700

    :white_check_mark: Added tests for theme select on init

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,6 @@ module.exports = {
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
         alias: {
-          "~": path.resolve(__dirname, "src"),
           "@": path.resolve(__dirname, "src"),
         }
     },
