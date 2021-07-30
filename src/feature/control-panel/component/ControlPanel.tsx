7b2c555f0e1b67ea045a1b55d672b29fddd9a8c8
Joseph R Miles <joe@jrm.dev>
2021-07-30 13:28:27 -0700

    :lipstick: ControlPanel now floats like before

--- src/feature/control-panel/component/ControlPanel.tsx
+++ src/feature/control-panel/component/ControlPanel.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-import styled from "@emotion/styled";
+import { Box, Container, css } from "@chakra-ui/react";
 
 import { ActorPanel } from "@/feature/actor";
 import { SnapToGrid } from "@/feature/grid-snap";
@@ -8,30 +7,34 @@ import { ThemeSelect } from "@/feature/theme-select";
 
 export const controlPanelTestId = "control-panel";
 
-const ControlPanelContainer = styled.div`
-  align-self: start;
-  justify-self: end;
-
-  position: absolute;
-  z-index: 1;
-
-  right: 0;
-  margin: 1em 1em 0 0;
-`;
-
 export const ControlPanel = () =>
 (
-  <ControlPanelContainer>
-    <section>
-      <ActorPanel />
-    </section>
-    <section>
-      <SnapToGrid />
-    </section>
-    <section>
-      <ThemeSelect />
-    </section>
-  </ControlPanelContainer>
+  <Container
+    data-testid={controlPanelTestId}
+    css={css`
+      align-self: start;
+      justify-self: end;
+
+      position: absolute;
+      z-index: 1;
+
+      top: 0;
+      right: 0;
+      margin: 1em 1em 0 0;
+    `}
+  >
+    <Box bg="white" p={4} borderRadius="sm">
+      <section>
+        <ActorPanel />
+      </section>
+      <section>
+        <SnapToGrid />
+      </section>
+      <section>
+        <ThemeSelect />
+      </section>
+    </Box>
+  </Container>
 );
 
 export default ControlPanel;
