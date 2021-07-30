5aa6c683555ce87fd580c2c686e1b2700729d07d
Joseph R Miles <joe@jrm.dev>
2021-07-30 13:50:06 -0700

    :lipstick: Added css reset styles to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import App from "./App";
 
 import "./index.css";
 
 ReactDOM.render(
   <ChakraProvider>
+    <CSSReset />
     <App />
   </ChakraProvider>,
   document.getElementById("app")
