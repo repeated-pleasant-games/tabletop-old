17b3333eb4d803b73e5d1b9970373c867815ee0b
Joseph R Miles <joe@jrm.dev>
2021-07-30 14:29:30 -0700

    :lipstick: Set theme to use system color mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
 import App from "./App";
 
+const theme = extendTheme({
+  config: {
+    cssVarPrefix: "rpg",
+    initialColorMode: "light",
+    useSystemColorMode: true,
+  }
+})
+
 ReactDOM.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <CSSReset />
     <App />
   </ChakraProvider>,
