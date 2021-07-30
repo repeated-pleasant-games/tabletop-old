38368e43ab7702f63454060c2db8b56b77bf4490
Joseph R Miles <joe@jrm.dev>
2021-07-30 13:57:55 -0700

    :fire: Removed all css modules and utility components

--- src/component/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import styles from "./Button.module.css";
-
-export const Button = ({ children, onClick }: React.HTMLAttributes<{}>) =>
-(
-  <button className={styles.button} onClick={onClick}>{children}</button>
-);
--- types/css.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "*.module.css" {
-  const content: {
-    [className: string]: string
-  };
-  export default content;
-}
