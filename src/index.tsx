d53b2263c970c75c4cc74a13c02edb49de634d8a
Joseph R Miles <joe@jrm.dev>
2021-07-30 13:50:32 -0700

    :fire: Removed import of index.css

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import * as ReactDOM from "react-dom"
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import App from "./App";
 
-import "./index.css";
-
 ReactDOM.render(
   <ChakraProvider>
     <CSSReset />
