f4adaef02a44c8c04b2aaacc3f1820aefa9f3273
Joseph R Miles <joe@jrm.dev>
2021-08-04 13:00:06 -0700

    :fire: Removed theme logic

--- src/feature/store-local/index.ts
+++ src/feature/store-local/index.ts
@@ -2,10 +2,9 @@ import create from "zustand";
 
 import { identityTransform, Transform } from "@/lib/Transform";
 import { createGridSnapState, GridSnapState } from "@/feature/grid-snap";
-import { createThemeState, ThemeState } from "@/feature/theme-select";
 import { createRoomState, RoomState } from "@/feature/room";
 
-export type LocalState = GridSnapState & ThemeState & RoomState &
+export type LocalState = GridSnapState & RoomState &
 {
   viewTransform: Transform,
   setViewTransform: (viewTransform: Transform) => void,
@@ -15,7 +14,6 @@ export const useLocalStore = create<LocalState>(
   (set) =>
   ({
     ...createGridSnapState(set),
-    ...createThemeState(set),
     ...createRoomState(set),
 
     viewTransform: identityTransform(),
