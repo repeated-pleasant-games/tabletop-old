450c76a669875592a76646567375f668f934102f
Joseph R Miles <joe@jrm.dev>
2021-08-04 12:59:03 -0700

    :wrench: Cleaned up webpack config spacing

--- src/preload.ts
+++
@@ -1,9 +0,0 @@
-import { detectAndSetTheme } from "@/feature/theme-select";
-
-detectAndSetTheme();
-
-window.matchMedia("(prefers-color-scheme: dark)")
-.addEventListener(
-  "change",
-  detectAndSetTheme
-);
