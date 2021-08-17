441bc141576c50f4dccb0ccd9a39de2929307ee8
Joseph R Miles <joe@jrm.dev>
2021-08-17 13:30:29 -0700

    :recycle: Setup tabletop to use things with position attributes

--- src/feature/tabletop/component/Tabletop.tsx
+++ src/feature/tabletop/component/Tabletop.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
 
-import { Actors } from "@/feature/actor";
 import styled from "@emotion/styled";
+
+import {
+  Attribute,
+  ThingProvider,
+  useThing,
+  useThingAttributeSystem
+} from "@/feature/thing-attribute-system";
+
+import { Token } from "./Token";
 
 const TabletopContainer = styled.svg`
   place-self: start;
@@ -9,16 +17,72 @@ const TabletopContainer = styled.svg`
   height: 100vh;
 `;
 
+type PositionAttribute = Attribute<"position"> &
+{
+  x: number,
+  y: number,
+};
+
+const TokenAdapter = () => 
+{
+  const { getAttributeByType } = useThing();
+
+  const { x, y } = getAttributeByType<PositionAttribute>("position");
+
+  return (
+    <Token x={x} y={y} cellSize={16} setPosition={() => void null} />
+  )
+};
+
 type TabletopProps = React.HTMLAttributes<{}> &
 {
   grid: React.ReactElement,
 };
 export const Tabletop = ({ grid, }: TabletopProps) =>
-(
-  <TabletopContainer>
-    {grid}
-    <Actors />
-  </TabletopContainer>
-);
+{
+  const {
+    getThingsWithAttributeTypes,
+    createThing,
+    addAttributeToThing
+  } = useThingAttributeSystem();
+
+  const thingIds = React.useMemo(
+    () =>
+      getThingsWithAttributeTypes<[ Attribute<"position"> ]>("position"),
+    [ getThingsWithAttributeTypes ]
+  );
+
+  React.useEffect(
+    () =>
+    {
+      const thing = createThing();
+      addAttributeToThing<PositionAttribute>(
+        thing,
+        {
+          type: "position",
+          x: 0,
+          y: 0,
+        }
+      );
+    },
+    []
+  );
+
+  return (
+    <TabletopContainer>
+      {grid}
+      {
+        thingIds.map(
+          (id) =>
+          (
+            <ThingProvider key={id} thingId={id}>
+              <TokenAdapter />
+            </ThingProvider>
+          )
+        )
+      }
+    </TabletopContainer>
+  );
+}
 
 export default Tabletop;
