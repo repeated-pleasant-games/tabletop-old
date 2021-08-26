e9ab8e730539a3c973ffc9a9f84280ba61ac3b5d
Joseph R Miles <joe@jrm.dev>
2021-08-26 14:03:11 -0700

    :sparkles: Cursors look like cursors now

--- src/feature/user-awareness/component/Cursor.tsx
+++ src/feature/user-awareness/component/Cursor.tsx
@@ -2,10 +2,21 @@ import React from "react";
 
 export const Cursor = ({ x, y }: { x: number, y: number}) =>
 (
-  <rect
-    x={x} y={y}
-    width={10}
-    height={10}
-    fill={"red"}
-  />
+  <g
+    transform={`
+      translate(${x},${y})
+      scale(0.025)
+      matrix(1.82605,-0.803689,0.803689,1.82605,-531.498,-59.7769)
+    `}>
+    <path
+      d="
+        M 120.099,388.586
+        L 231.762,134.739
+        L 343.424,388.586
+        L 231.762,355.867
+        L 120.099,388.586
+        Z
+      "
+    />
+  </g>
 );
